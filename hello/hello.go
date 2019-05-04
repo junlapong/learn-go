46c31583cea5576baae7aa5348615be55d483eb7
Junlapong L <junlapong@gmail.com>
2019-05-04 21:02:58 +0700

    hello test

---
+++ hello/hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const enHelloPrefix = "Hello, "
+const spHelloPrefix = "Hola, "
+const frHelloPrefix = "Bonjour, "
+
+// Hello world
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case "french":
+		prefix = frHelloPrefix
+	case "spanish":
+		prefix = spHelloPrefix
+	default:
+		prefix = enHelloPrefix
+	}
+
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("Jun", ""))
+}
---
+++ hello/hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessge := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jun", "french")
+		want := frHelloPrefix + "Jun"
+
+		assertCorrectMessge(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'Worls'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessge(t, got, want)
+	})
+}
