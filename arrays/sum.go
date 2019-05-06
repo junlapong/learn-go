9dc9877850618ffd32443afc9e09e59ec334b4d8
Junlapong L <junlapong@gmail.com>
2019-05-06 16:08:03 +0700

    add SumAll()

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,3 +9,14 @@ func Sum(numbers []int) (sum int) {
 
 	return
 }
+
+// SumAll numbers
+func SumAll(numbersToSum ...[]int) (sums []int) {
+
+	sums = make([]int, len(numbersToSum))
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return
+}
