92e0fbc969b1ab87f0e6fedb10273996c30a4616
Junlapong L <junlapong@gmail.com>
2019-05-06 17:41:52 +0700

    add SumAllTails()

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,7 @@
 package arrays
 
+import "fmt"
+
 // Sum nummers
 func Sum(numbers []int) (sum int) {
 
@@ -13,9 +15,25 @@ func Sum(numbers []int) (sum int) {
 // SumAll numbers
 func SumAll(numbersToSum ...[]int) (sums []int) {
 
-	sums = make([]int, len(numbersToSum))
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	// sums = make([]int, len(numbersToSum))
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+// SumAllTails numbers
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		fmt.Printf("tail: %v\n", tail)
+		sums = append(sums, Sum(tail))
 	}
 
 	return
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -27,4 +28,27 @@ func TestSumAll(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSlice(t *testing.T) {
+	numbers := []int{0, 1, 2, 3, 4}
+	fmt.Printf("%v => len: %d, cap: %d\n", numbers, len(numbers), cap(numbers))
+	fmt.Printf("%v\n", numbers[1:])
+	fmt.Printf("%v\n", numbers[1:3])
+
+	b := []byte{'g', 'o', 'l', 'a', 'n', 'g'}
+	fmt.Printf("%c\n", b)
+	fmt.Printf("%c\n", b[:2])
+	fmt.Printf("%c\n", b[1:4])
+
+	// TODO: len, cap
 }
