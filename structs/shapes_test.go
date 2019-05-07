03231dcb59828827d3a76a10d6c005cfb24419f5
Junlapong L <junlapong@gmail.com>
2019-05-07 16:11:55 +0700

    add Table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -63,5 +63,23 @@ func TestArea(t *testing.T) {
 		circle := Circle{10}
 		checkArea(t, circle, 314.1592653589793)
 	})
+}
+
+func TestAreaTables(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
 
 }
